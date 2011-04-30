da008c083363f5b663501114693df4dafaa66922
Simone Carletti <weppos@weppos.net>
2011-04-30 22:54:48 +0200

    Contact comments and notes.

--- lib/free_agent/contact.rb
+++ lib/free_agent/contact.rb
@@ -2,20 +2,32 @@ module FreeAgent
 
   # Represents a Contact in FreeAgent.
   #
-  # == Methods
-  #
-  # * .all
-  # * .first
-  # * .last
-  # * .find(id)
-  # * .new
-  # * .create
-  #
-  # * #save
-  # * #update
-  # * #destroy
+  # @attr [String] first_name
+  # @attr [String] last_name
+  # @attr [String] organisation_name
   #
   class Contact < Base
+
+    # == Methods
+    #
+    # * .all
+    # * .first
+    # * .last
+    # * .find(id)
+    # * .new
+    # * .create
+    #
+    # * #save
+    # * #update
+    # * #destroy
+    #
+    # == Relationships
+    #
+    # has_many :bills
+    # has_many :estimates
+    # has_many :projects
+    # has_many :invoices
+
 
     validates_presence_of :first_name, :unless => :organisation_name?
     validates_presence_of :last_name,  :unless => :organisation_name?
