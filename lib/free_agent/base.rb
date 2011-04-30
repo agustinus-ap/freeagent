91c2d5ad5a29a80a8fb6368e3e8711ac919ee66b
Simone Carletti <weppos@weppos.net>
2011-04-30 20:55:21 +0200

    Dynamic Finders feature.

--- lib/free_agent/base.rb
+++ lib/free_agent/base.rb
@@ -1,6 +1,138 @@
 module FreeAgent
 
+  # The Base class for any FreeAgent resource.
+  #
+  # == Dynamic Finders
+  #
+  # Like ActiveRecord, this class support dynamic finders.
+  # You can use dynamic finders to search a resource by any of its attributes.
+  #
+  #   class Contact < Base
+  #   end
+  #
+  #   Contact.find_by_email 'email@example.org'
+  #   Contact.find_by_username_and_email 'weppos', 'email@example.org'
+  #
+  # However, there's one important caveat we should mention.
+  # This feature is not very efficient and the more records you have,
+  # the longer the query it takes.
+  #
+  # This is because record filtering is performed client-side.
+  # The library first loads and instantiate all records, then it filters them
+  # applying given criteria.
+  #
   class Base < ActiveResource::Base
+
+    # = Active Record Dynamic Finder Match
+    #
+    # Refer to ActiveRecord::Base documentation for Dynamic attribute-based finders for detailed info.
+    #
+    class DynamicFinderMatch
+      def self.match(method)
+        finder       = :first
+        bang         = false
+        instantiator = nil
+
+        case method.to_s
+        when /^find_(all_|last_)?by_([_a-zA-Z]\w*)$/
+          finder = :last if $1 == 'last_'
+          finder = :all if $1 == 'all_'
+          names = $2
+        when /^find_by_([_a-zA-Z]\w*)\!$/
+          bang = true
+          names = $1
+        when /^find_or_(initialize|create)_by_([_a-zA-Z]\w*)$/
+          instantiator = $1 == 'initialize' ? :new : :create
+          names = $2
+        else
+          return nil
+        end
+
+        new(finder, instantiator, bang, names.split('_and_'))
+      end
+
+      def initialize(finder, instantiator, bang, attribute_names)
+        @finder          = finder
+        @instantiator    = instantiator
+        @bang            = bang
+        @attribute_names = attribute_names
+      end
+
+      attr_reader :finder, :attribute_names, :instantiator
+
+      def finder?
+        @finder && !@instantiator
+      end
+
+      def instantiator?
+        @finder == :first && @instantiator
+      end
+
+      def creator?
+        @finder == :first && @instantiator == :create
+      end
+
+      def bang?
+        @bang
+      end
+    end
+
+
+    class << self
+
+      protected
+
+        def method_missing(method_id, *arguments, &block)
+          if match = DynamicFinderMatch.match(method_id)
+            attribute_names = match.attribute_names
+            if match.finder?
+              find_by_attributes(match, attribute_names, *arguments)
+            elsif match.instantiator?
+              find_or_instantiator_by_attributes(match, attribute_names, *arguments, &block)
+            end
+          else
+            super
+          end
+        end
+
+        def find_by_attributes(match, attributes, *args)
+          conditions = Hash[attributes.map {|a| [a, args[attributes.index(a)]]}]
+          result = all.select do |record|
+            conditions.all? { |k,v| record.send(k) == v }
+          end
+          result = result.send(match.finder) if match.finder != :all
+
+          if match.bang? && result.blank?
+            raise ActiveResource::ResourceNotFound, 404, "Couldn't find #{self} with #{conditions.to_a.collect {|p| p.join(' = ')}.join(', ')}"
+          else
+            result
+          end
+        end
+
+        def find_or_instantiator_by_attributes(match, attributes, *args)
+          record = find_by_attributes(match, attributes, *args)
+
+          unless record
+            conditions = Hash[attributes.map {|a| [a, args[attributes.index(a)]]}]
+            record = new(conditions)
+            yield(record) if block_given?
+            record.save if match.instantiator == :create
+          end
+
+          record
+        end
+
+    end
+
+
+    def respond_to?(method_id, include_private = false)
+      if DynamicFinderMatch.match(method_id)
+        return true
+      end
+
+      super
+    end
+
   end
 
 end
--- spec/unit/base_spec.rb
+++ spec/unit/base_spec.rb
@@ -6,4 +6,117 @@ describe FreeAgent::Base do
     klass.superclass.should == ActiveResource::Base
   end
 
+
+  describe "DynamicFinderMatch" do
+
+    class Record < FreeAgent::Base
+    end
+
+    before(:each) do
+      @records = [
+        Record.new(:username => "alpha", :email => "alpha@example.org", :locale => "en"),
+        Record.new(:username => "beta", :email => "beta@example.org", :locale => "en"),
+        Record.new(:username => "charlie", :email => "charlie@example.org", :locale => "jp"),
+        Record.new(:username => "delta", :email => "delta@example.org", :locale => "en"),
+        Record.new(:username => "beta", :email => "beta@example.org", :locale => "jp"),
+      ]
+      stub(Record).all { @records }
+    end
+
+    describe "#find_by_" do
+      it "fetches .all records" do
+        mock(Record).all { @records }
+        Record.find_by_username('test')
+      end
+
+      it "searches and returns the first item matching criteria" do
+        Record.find_by_locale('en').should == @records[0]
+      end
+
+      it "supports multiple criteria" do
+        Record.find_by_email_and_username('beta@example.org', 'beta').should == @records[1]
+        Record.find_by_username_and_email('beta', 'beta@example.org').should == @records[1]
+      end
+    end
+
+    describe "#find_or_initialize_by_" do
+      it "fetches .all records" do
+        mock(Record).all { @records }
+        Record.find_or_initialize_by_username('test')
+      end
+
+      it "searches and returns the first item matching criteria" do
+        Record.find_or_initialize_by_username('en').should == @records[0]
+      end
+
+      it "supports multiple criteria" do
+        Record.find_or_initialize_by_email_and_username('beta@example.org', 'beta').should == @records[1]
+        Record.find_or_initialize_by_username_and_email('beta', 'beta@example.org').should == @records[1]
+      end
+
+      it "initializes the record if not available" do
+        record = Record.find_or_initialize_by_email('hello@example.org', 'hello')
+        record.email.should == 'hello@example.org'
+
+        record = Record.find_or_initialize_by_username_and_email('hello', 'hello@example.org')
+        record.username.should == 'hello'
+        record.email.should == 'hello@example.org'
+      end
+    end
+
+    describe "#find_all_by_" do
+      it "fetches .all records" do
+        mock(Record).all { @records }
+        Record.find_all_by_username('test')
+      end
+
+      it "searches and returns all items matching criteria" do
+        Record.find_all_by_locale('en').should == [@records[0], @records[1], @records[3]]
+      end
+
+      it "supports multiple criteria" do
+        Record.find_all_by_email_and_username('beta@example.org', 'beta').should == [@records[1], @records[4]]
+        Record.find_all_by_username_and_email('beta', 'beta@example.org').should == [@records[1], @records[4]]
+      end
+    end
+
+    describe "#find_last_by_" do
+      it "fetches .all records" do
+        mock(Record).all { @records }
+        Record.find_last_by_username('test')
+      end
+
+      it "searches and returns the last item matching criteria" do
+        Record.find_last_by_locale('en').should == @records[3]
+      end
+
+      it "supports multiple criteria" do
+        Record.find_last_by_email_and_username('beta@example.org', 'beta').should == @records[4]
+        Record.find_last_by_username_and_email('beta', 'beta@example.org').should == @records[4]
+      end
+    end
+
+    describe "#respond_to?" do
+      it "responds to find_by_*" do
+        Record.new.should respond_to(:find_by_name)
+        Record.new.should respond_to(:find_by_name_and_email)
+      end
+
+      it "responds to find_or_initialize_by_*" do
+        Record.new.should respond_to(:find_or_initialize_by_name)
+        Record.new.should respond_to(:find_or_initialize_by_name_and_email)
+      end
+
+      it "responds to find_all_by_*" do
+        Record.new.should respond_to(:find_all_by_name)
+        Record.new.should respond_to(:find_all_by_name_and_email)
+      end
+
+      it "responds to find_last_by_*" do
+        Record.new.should respond_to(:find_last_by_name)
+        Record.new.should respond_to(:find_last_by_name_and_email)
+      end
+    end
+  end
+
 end
